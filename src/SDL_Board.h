14027713b7814adb65635f9dfc64c90fcdfda1d8
Matthew Sparkes <matthew.sparkes@york.ac.uk>
2022-10-11 10:44:06 +0100

    Added functionality for overlay

--- src/SDL_Board.h
+++ src/SDL_Board.h
@@ -36,6 +36,9 @@ enum BOARD_VALUES
     SQUARE_BLACK_R = 117,
     SQUARE_BLACK_G = 79,
     SQUARE_BLACK_B = 18,
+    SQUARE_GREEN_R = 130,
+    SQUARE_GREEN_G = 180,
+    SQUARE_GREEN_B = 126,
     SQUARE_WIDTH = 75,
     SQUARE_HEIGHT = 75,
     BOARD_X = 20,
@@ -48,24 +51,31 @@ enum BOARD_VALUES
 class SDL_Board
 {
     protected:
+        // MEMBER VARIABLES
         SDL_Window* window;                     // Window handle
         SDL_Renderer* windowRenderer;           // Window renderer
-        int SDL_coordinates[8][8][2];           // Chess board coordinates list: 8 by 8 coordiates (top left to bottom right) and x,y window coordinates of square       
+        int SDL_coordinates[8][8][2];           // Chess board coordinates list: 8 by 8 coordiates (top left to bottom right) and x,y window coordinates of square
+
+        // INTERNAL LOADING METHODS
+        void loadBackground();                   
+        void loadPiece(int x, int y, std::string filePath);
+        void loadBoard(BoardState state);
+        void loadSquare(int x, int y);
+        void clearBoard();
+        SDL_Rect returnSquare(int x, int y);
         
-        
+                
     public: 
         SDL_Board();                            // Constructor
         ~SDL_Board();                           // Destructor
         
-        SDL_Rect Get_BoardSquare(int x, int y); 
-
-        void method();
-        void GenerateBoard();                   // Handle initial board generation 
-        void RenderPiece(int x, int y, std::string filePath);
-        void renderCurrentState(BoardState state);
-        void renderNewState(BoardState state);
-        void ClearSquare(int x, int y);
-        void ClearBoard();
+        // PUBLIC RENDER METHODS
+        void renderBackground();
+        void renderNewBoard(BoardState state);
+        void renderBoardUpdate(BoardState state);
+        void renderOverlay(int x, int y);
+    
         
+        // STATIC METHODS
         static BoardPosition SDL_to_Coords(int SDL_x, int SDL_y);
 };
